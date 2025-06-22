d70522f6b3cd77697dea4a1ea6a45dee6e97ba4a
Joe Kay <joelkugonza@gmail.com>
2025-06-22 07:51:32 +0300

    add js

--- hamburger.js
+++ hamburger.js
@@ -4,3 +4,10 @@ harmburger.addEventListener('click', ()=>{
     harmburger.classList.toggle('active')
     navMenu.classList.toggle('active')
 })
+
+document.querySelectorAll('.nav-link').forEach((item)=>{
+    item.addEventListener('click',()=>{
+        harmburger.classList.remove('active')
+        navMenu.classList.remove('active')
+    })
+})
